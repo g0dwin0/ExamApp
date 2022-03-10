738db4da8f546c4c72a821ed11af7e00db26f1c3
ddd aaa <21052794@student.hhs.nl>
2022-03-10 15:23:35 +0100

    added gson

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,6 +1,11 @@
+import com.google.gson.Gson;
+
 public class Run {
     public static void main(String[] args) {
+        Gson serializer = new Gson();
         Student godwin = new Student("Godwin");
-        new Exam().startExam(godwin);
+        String json = serializer.toJson(godwin);
+        System.out.println(json);
+//        new Exam().startExam(godwin);
     }
 }
