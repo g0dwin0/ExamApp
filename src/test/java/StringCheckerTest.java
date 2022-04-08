707e95a3fd2d6dcc098ad02dedc9b784b32398a6
Rowin <rowinhartog@outlook.com>
2022-04-08 16:59:38 +0200

    StringCheckerTest Complete

---
+++ src/test/java/StringCheckerTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class StringCheckerTest {
+
+    private StringChecker stringCheckerUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        stringCheckerUnderTest = new StringChecker();
+    }
+
+    @Test
+    void testIsFullName_true() {
+        assertTrue(stringCheckerUnderTest.isFullName("Rowin Hartog"));
+    }
+    @Test
+    void testIsFullName_Partly(){
+        assertFalse(stringCheckerUnderTest.isFullName("Rowin"));
+    }
+    @Test
+    void testIsFullName_forbidden_characters(){
+        assertFalse(stringCheckerUnderTest.isFullName("3244% 9ah"));
+    }
+
+    @Test
+    void testIsFullName_forbidden_numbers(){
+        assertFalse(stringCheckerUnderTest.isFullName(("R0win H4rtog")));
+        //Numbers are still allowed
+
+    }
+    @Test
+    void testIsStudentNumber() {
+        assertTrue(stringCheckerUnderTest.isStudentNumber("19092113"));
+    }
+    @Test
+    void testIsStudentNumber_letters(){
+        assertFalse(stringCheckerUnderTest.isStudentNumber("19O22113"));
+        // Letters still allowed
+    }
+    @Test
+    void testIsStudentNumber_characters(){
+        assertFalse(stringCheckerUnderTest.isStudentNumber("$19092!!3"));
+        // Characters still allowed
+    }
+    @Test
+    void testIsStudentNumber_short(){
+        assertFalse(stringCheckerUnderTest.isStudentNumber("1234"));
+    }
+}
