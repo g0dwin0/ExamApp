e8f3a1f1bef54e694178cb9d51472c859220da57
Rowin <rowinhartog@outlook.com>
2022-04-09 01:05:11 +0200

    studenttest

---
+++ src/test/java/examapp/StudentTest.java
@@ -0,0 +1,74 @@
+package examapp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StudentTest {
+    private Student studentUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        studentUnderTest = new Student("Rowin Hartog", "19092113");
+        final Result result = new Result(new Exam("name", new ArrayList<>(
+                List.of(new Question("question", new ArrayList<>(List.of("value")), "answer", 100)))), 10, true);
+        final Result result1 = new Result(new Exam("name", new ArrayList<>(
+                List.of(new Question("question", new ArrayList<>(List.of("value")), "answer", 100)))), 10, false);
+        studentUnderTest.addResult(result);
+        studentUnderTest.addResult(result1);
+    }
+    @Test
+    void testGetNaam() {
+        assertEquals("Rowin Hartog", studentUnderTest.getStudentName());
+    }
+
+    @Test
+    void testGetStudentnummer() {
+        assertEquals("19092113", studentUnderTest.getStudentnummer());
+    }
+
+    @Test
+    void testSetNaam() {
+
+        studentUnderTest.setNaam("Rowin Hartogg");
+        assertEquals("Rowin Hartogg",studentUnderTest.getStudentName());
+    }
+
+    @Test
+    void testAddResult() {
+
+        final Result result = new Result(new Exam("cxzxcxxc", new ArrayList<>(
+                List.of(new Question("question", new ArrayList<>(List.of("value")), "answer", 100)))), 10, true);
+
+        studentUnderTest.addResult(result);
+        Assertions.assertEquals(result,studentUnderTest.getResult(result));
+    }
+
+    @Test
+    void testGetAmountSuccesses() {
+        final int outcome = studentUnderTest.getAmountSuccesses();
+
+        assertEquals(1, outcome);
+    }
+
+    @Test
+    void testGetResult() {
+        // Setup
+        final Result result = new Result(new Exam("name1", new ArrayList<>(
+                List.of(new Question("question", new ArrayList<>(List.of("value")), "answer", 0)))), 12, true);
+        studentUnderTest.addResult(result);
+
+        final Result result2 = studentUnderTest.getResult(result);
+        Assertions.assertEquals(result2,studentUnderTest.getResult(result));
+        assertTrue(studentUnderTest.getStudentResultsList().get(2).passed);
+        assertEquals("name1",studentUnderTest.getStudentResultsList().get(2).getExam().getName());
+        assertEquals(12,studentUnderTest.getStudentResultsList().get(2).getAmountCorrect());
+
+    }
+}
