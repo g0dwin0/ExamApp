36140b4210f734dbb9c7421cef5c7ea1900ab101
JanssenTimo <dhrtjanssen@gmail.com>
2022-04-13 19:56:20 +0200

    ExamTest

--- src/test/java/ExamTest.java
+++ src/test/java/ExamTest.java
@@ -1,47 +1,34 @@
+import examapp.Exam;
+import examapp.Question;
 import junit.framework.TestCase;
 import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class ExamTest extends TestCase {
 
-    public void testStartExam() {
+    public void testExam() {
+        ArrayList<Question> questions = new ArrayList<>();
+        Exam exam = new Exam("Taaltoets", questions);
+        assertEquals("Taaltoets", exam.getName());
+        assertEquals(questions, exam.getQuestions());
     }
 
-    public void testFinalCijfer() {
+    public void testEmptyExam() {
+        Exam exam = new Exam();
+        assertEquals("", exam.getName());
+        assertNull(exam.getQuestions());
     }
 
-    public void testGetNaam() {
+    public void testGetName() {
         Exam exam = new Exam("Security",null);
         Assert.assertEquals("Security",exam.getName());
     }
 
-    public void testAddExam() {
-
-    }
-
-    public void testSetNaam() {
-    }
-
-    public void testGetAantalJuisteAntwoorden() {
-    }
-
-    public void testSetAantalJuisteAntwoorden() {
-    }
-
-    public void testGetVragen() {
-    }
-
-    public void testSetVragen() {
-    }
-
-    public void testGetResultaten() {
-    }
-
-    public void testSetResultaten() {
-    }
-
-    public void testGetMinimumCorrect() {
-    }
-
-    public void testSetMinimumCorrect() {
+    public void testGetQuestions() {
+        ArrayList<Question> questions = new ArrayList<>();
+        Exam exam = new Exam("Exam", questions);
+        assertEquals(questions, exam.getQuestions());
     }
 }
